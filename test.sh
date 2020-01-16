8df425ad8efdd67fc0efd3d558cd3763726b84f6
Rishi Gargi <gargi.rishi@gmail.com>
2020-01-16 10:56:28 +0530

    init

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-mkdir test
-cd test
-echo "hello world" > test.txt
+rm -rf cred.json
+echo '{"topicPrefix": "/tfyb/15/5e08c5441bcce26c780ec105/","username": "15","password": "120ytf5e08c5441bcce26c780ec10514987"}' > cred.json
+pm2 restart 0
