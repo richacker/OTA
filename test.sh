24ddb5c60982e19b1aad39995665a2c514538662
Rishi Gargi <gargi.rishi@gmail.com>
2020-01-16 10:59:07 +0530

    init

--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
-rm -rf cred.json
-echo '{"topicPrefix": "/tfyb/15/5e08c5441bcce26c780ec105/","username": "15","password": "120ytf5e08c5441bcce26c780ec10514987"}' > cred.json
-pm2 restart 0
+echo '{"topicPrefix": "/tfyb/15/5e08c5441bcce26c780ec105/","username": "15","password": "120ytf5e08c5441bcce26c780ec10514987"}' > cred2.json
